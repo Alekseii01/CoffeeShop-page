b7c17fdf146cfa4001ae821633dd168961a91c33
Aleksei <aleksei.karpukovich@student.ehu.lt>
2024-05-29 15:26:44 +0300

    fix: testimonial-slider width

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const testimonialSlide = document.querySelector('.testimonial-slide');
 const currentSlideIndicator = document.getElementById('current-slide');
 const progressBar = document.querySelector('.brown-bar');
 
-let currentIndex = 1; 
+if (document.documentElement.clientWidth < 1000) {
+  currentSlideIndicator.textContent = 1;
+}
+
+let currentIndex = 0; 
 
 prevButton.addEventListener('click', () => {
     updateIndex(-1); // Move back by one slide
@@ -22,105 +26,21 @@ function updateIndex(delta) {
     if (currentIndex < 1) {
         currentIndex = 1;
     } else if (currentIndex >= totalSlides) {
-        currentIndex = totalSlides - 1;
+      if (document.documentElement.clientWidth > 1000) {
+        currentIndex = totalSlides -1;
+      } else {
+        currentIndex = totalSlides;
+      }
     }
 }
 
 function updateSlidePosition() {
     const slideWidth = testimonialSlide.children[0].getBoundingClientRect().width + 40;
     testimonialSlide.style.transform = `translateX(-${(currentIndex - 1) * slideWidth}px)`;
-    currentSlideIndicator.textContent = currentIndex + 1;
+    currentSlideIndicator.textContent = document.documentElement.clientWidth > 1000 ? currentIndex + 1 : currentIndex;
+
 
     const totalSlides = testimonialSlide.children.length;
-    const progressWidth = ((currentIndex) / (totalSlides - 1)) * 100;
+    const progressWidth = document.documentElement.clientWidth > 1000 ? ((currentIndex) / (totalSlides - 1)) * 100 : ((currentIndex - 1) / (totalSlides - 1)) * 100;
     progressBar.style.width = `${progressWidth}%`;
 }
-
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 40.674, lng: -73.945 },
-      zoom: 12,
-      styles: [
-        { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-        { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-        {
-          featureType: "administrative.locality",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "geometry",
-          stylers: [{ color: "#263c3f" }],
-        },
-        {
-          featureType: "poi.park",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#6b9a76" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [{ color: "#38414e" }],
-        },
-        {
-          featureType: "road",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#212a37" }],
-        },
-        {
-          featureType: "road",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#9ca5b3" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry",
-          stylers: [{ color: "#746855" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "geometry.stroke",
-          stylers: [{ color: "#1f2835" }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#f3d19c" }],
-        },
-        {
-          featureType: "transit",
-          elementType: "geometry",
-          stylers: [{ color: "#2f3948" }],
-        },
-        {
-          featureType: "transit.station",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#d59563" }],
-        },
-        {
-          featureType: "water",
-          elementType: "geometry",
-          stylers: [{ color: "#17263c" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.fill",
-          stylers: [{ color: "#515c6d" }],
-        },
-        {
-          featureType: "water",
-          elementType: "labels.text.stroke",
-          stylers: [{ color: "#17263c" }],
-        },
-      ],
-    });
-  }
-  
-  window.initMap = initMap;
